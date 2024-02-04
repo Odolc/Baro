687bfc356785a5d9c257191683f17a3f88381591
JEALG <jalain.gosse@icloud.com>
2024-02-04 09:30:20 +0100

    suppression Lien commauty

--- desktop/php/baro.php
+++ desktop/php/baro.php
@@ -24,13 +24,19 @@ $eqLogics = eqLogic::byType($plugin->getId());
             </div>
         </div>
         <!-- Champ de recherche -->
-        <div class="input-group" style="margin-bottom:5px;">
-            <input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
-            <div class="input-group-btn">
-                <a id="bt_resetObjectSearch" class="btn" style="width:30px"><i class="fas fa-times"></i>
-                </a><a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
-            </div>
-        </div>
+        <?php
+        if (count($eqLogics) == 0) {
+        } else {
+            // Champ de recherche
+            echo '<div class="input-group" style="margin:5px;">';
+            echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic">';
+            echo '<div class="input-group-btn">';
+            echo '<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>';
+            echo '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
+            echo '</div>';
+            echo '</div>';
+        }
+        ?>
         <!-- Liste des équipements du plugin -->
         <legend><i class="fas fa-chart-bar"></i> {{Mes Tendances Baro}}</legend>
         <div class="eqLogicThumbnailContainer">
